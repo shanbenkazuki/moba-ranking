987ef3c07e43e1f82bbef27af9b831e1670bbbeb
Shanben Kazuki <74257571+shanbenkazuki@users.noreply.github.com>
2022-02-27 16:01:02 +0900

    ・BAN率追加 ・エラー解消

--- mlbb/scrMlbbBanRateRank.py
+++ mlbb/scrMlbbBanRateRank.py
@@ -1,5 +1,5 @@
 import time
-import mlbb.convMlbbHero as convMlbbHero
+import convMlbbHero as convMlbbHero
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -17,8 +17,17 @@ profileOptions.add_argument(profilefolder)
 driver = webdriver.Chrome(options = profileOptions)
 driver.get(loginUrl)
 
-#ランキング昇順
+#変換されるまで待機
+time.sleep(2)
+
+#ミシック400ポイント表示に切り替え
 driver.find_element(by=By.XPATH, value="//*[@id='rank']/div[1]/div[2]/ul/li[3]").click()
+
+#変換されるまで待機
+time.sleep(2)
+
+#BAN率の昇順表示に切り替え
+driver.find_element(by=By.XPATH, value="//*[@id='rank']/ul/li[7]").click()
 
 #変換されるまで待機
 time.sleep(2)
@@ -29,10 +38,10 @@ html = driver.page_source
 #ランキング抽出
 soup = BeautifulSoup(html, 'html.parser')
 
-#勝率が高いヒーロー10体の名前を取得
+#BAN率が高いヒーロー10体の名前を取得
 allHeroList = soup.select(".slotwrapper > ul > li > a > div:nth-of-type(1) > span", limit=10)
-#勝率が高いヒーロー10体の勝率を取得
-winRateList = soup.select(".slotwrapper > ul > li > a > div:nth-of-type(2)", limit=10)
+#BAN率が高いヒーロー10体の勝率を取得
+banRateList = soup.select(".slotwrapper > ul > li > a > div:nth-of-type(4)", limit=10)
 #統計日を取得
 statisticsDate = soup.select_one(".time").string
 
@@ -40,10 +49,10 @@ statisticsDate = soup.select_one(".time").string
 print("* 統計参照時期：''" + statisticsDate + "''")
 print("[-]")
 print("|CENTER|CENTER|c")
-print("|ヒーロー|COLSORT:勝率|h")
+print("|ヒーロー|COLSORT:BAN率|h")
 for i, hero in enumerate(allHeroList):
     hero = convMlbbHero.convertHeroName(hero.string)
-    print("|" + hero +"|"+ winRateList[i].string + "|")  
+    print("|" + hero +"|"+ banRateList[i].string + "|")  
 print("[END]")
 
 driver.close()
